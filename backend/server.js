6e57076c907dc1bcf55e30acef43c3c2208f9ec8
Tim Renken <quiptobasco@gmail.com>
2024-05-12 01:15:11 -0700

    more done2

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,9 @@ app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
 console.log(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+// });
 
 const connectedSockets = {};
 const gameStates = {};
