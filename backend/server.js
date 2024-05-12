fb77a26bf15b712791be658103afb2bddf52b414
Tim Renken <quiptobasco@gmail.com>
2024-05-12 00:20:29 -0700

    never3

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,9 @@ app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    res.sendFile(
+        path.join(__dirname, '..', '..', 'frontend', 'dist', 'index.html')
+    );
 });
 
 const connectedSockets = {};
